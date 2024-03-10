72492fa9d87eb75022b8605e6b9a4cc6191aa133
Celiel Castillo <celiel13@gmail.com>
2024-03-10 01:57:12 -0400

    Major improvements to code including About section

--- src/pages/About1.jsx
+++ src/pages/About1.jsx
@@ -19,10 +19,10 @@ function About1 () {
             <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
                 <div className="w-full lg:w-5/12 flex flex-col justify-center">
                     <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">Our team</h1>
-                    <p className="font-normal text-base leading-6 text-gray-600 ">Nuestro equipo siempre trabajo con las demograficas de mayor alcance.</p>
+                    <p className="font-normal text-base leading-6 text-gray-600 ">Nuestro equipo siempre trabaja con las demograficas de mayor alcance.</p>
                 </div>
                 <div className="w-full lg:w-8/12 lg:pt-8">
-                    <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
+                    <div className="grid md:grid-cols-2 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
                         <div className="p-2 pb-2 flex justify-center flex-col items-center">
                             <img className="md:block hidden" src="https://scontent.fsju1-1.fna.fbcdn.net/v/t1.6435-9/117971861_10158718326894432_4805530864384379242_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=5srlIWy4QPgAX9h9AM9&_nc_ht=scontent.fsju1-1.fna&oh=00_AfASZ266Didz32aXdVfMzst5_rLOGlEKl6zG5L26B1JaEA&oe=66149FF5" alt="Alexa featured Img" />
                             <img className="md:hidden block" src="https://scontent.fsju1-1.fna.fbcdn.net/v/t1.6435-9/117971861_10158718326894432_4805530864384379242_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=5srlIWy4QPgAX9h9AM9&_nc_ht=scontent.fsju1-1.fna&oh=00_AfASZ266Didz32aXdVfMzst5_rLOGlEKl6zG5L26B1JaEA&oe=66149FF5" alt="Alexa featured Img" />
