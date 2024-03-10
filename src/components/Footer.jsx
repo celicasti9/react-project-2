da3b43186f37ea9e8d3a202f0f4f7a4ee13a716b
Celiel Castillo <celiel13@gmail.com>
2024-03-10 00:08:17 -0400

    Fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,19 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="fixed bottom-0 w-full bg-white flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
-      <Typography color="blue-gray" className="font-normal">
-        &copy; 2024 Yunque 92.9 FM
-      </Typography>
       <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+      <li>
+          <Link to="/aboutus">
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              2024 Yunque 92.9 FM
+            </Typography>
+          </Link>
+        </li>
         <li>
           <Link to="/aboutus">
             <Typography
