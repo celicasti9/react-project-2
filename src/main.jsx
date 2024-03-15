1fdc3ee745ab8ff71e15deccb0a378da9b9c9891
Celiel Castillo <89181990+celicasti9@users.noreply.github.com>
2024-03-15 08:57:10 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import { ShowProvider } from './context/shows.context.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ShowProvider>
         <App />
-      </ShowProvider>
+
     </BrowserRouter>
   </React.StrictMode>
 );
